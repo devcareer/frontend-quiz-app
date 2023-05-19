199111bd0cd0571e5f40a284bff820b2a144eb6a
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2023-05-19 15:11:22 +0100

    Combined all three pages and added functionality to the quiz

---
+++ script.js
@@ -0,0 +1,159 @@
+// Get references to DOM elements
+const questionText = document.getElementById("question-text");
+const questionId = document.getElementById("question-id");
+const choicesContainer = document.querySelector(".choices--container");
+const choices = Array.from(document.querySelectorAll(".choices"));
+const summaryContainer = document.getElementById("summary-container");
+const mainBtn = document.getElementById("btn--main");
+const mainPage = document.getElementById("mainPage");
+const quizPage = document.getElementById("quizPage");
+const buttonContainer = document.getElementById("btn--container");
+
+mainBtn.addEventListener("click", () => {
+  mainPage.style.display = "none";
+  quizPage.style.display = "block";
+});
+
+// Store quiz data
+const quizData = [
+  {
+    id: 1,
+    question: "Which one of these is not like the other one?",
+    choices: ["A", "B", "C", "4"],
+    answer: 3,
+  },
+  {
+    id: 2,
+    question: "Which one of these is not like the other one?",
+    choices: ["1", "2", "3", "A"],
+    answer: 3,
+  },
+  {
+    id: 3,
+    question: "Which one of these is not like the other one?",
+    choices: ["Banana", "Dogs", "Monkey", "Cats"],
+    answer: 1,
+  },
+  {
+    id: 4,
+    question: "Which one of these is not like the other one?",
+    choices: ["A", "B", "C", "4"],
+    answer: 3,
+  },
+  {
+    id: 5,
+    question: "Which one of these is not like the other one?",
+    choices: ["1", "2", "3", "A"],
+    answer: 3,
+  },
+  {
+    id: 6,
+    question: "Which one of these is not like the other one?",
+    choices: ["Banana", "Dogs", "Monkey", "Cats"],
+    answer: 1,
+  },
+  {
+    id: 7,
+    question: "Which one of these is not like the other one?",
+    choices: ["A", "B", "C", "4"],
+    answer: 3,
+  },
+  {
+    id: 8,
+    question: "Which one of these is not like the other one?",
+    choices: ["1", "2", "3", "A"],
+    answer: 3,
+  },
+  {
+    id: 9,
+    question: "Which one of these is not like the other one?",
+    choices: ["Banana", "Dogs", "Monkey", "Cats"],
+    answer: 1,
+  },
+];
+
+let currentQuestion = 0;
+let score = 0;
+let summaryCounter = 0;
+
+// Function to load the current question
+function loadQuestion() {
+  const currentQuiz = quizData[currentQuestion];
+
+  // Update question text and id
+  questionText.textContent = currentQuiz.question;
+  questionId.textContent = currentQuiz.id;
+
+  // Update choices
+  choices.forEach((choice, index) => {
+    const choiceText = choice.querySelector(".choices__text");
+    choiceText.textContent = currentQuiz.choices[index];
+  });
+}
+
+function handleChoiceSelection(event) {
+  const selectedChoice = event.target.closest(".choices");
+  const currentQuiz = quizData[currentQuestion];
+  const selectedAnswer =
+    selectedChoice.querySelector(".choices__text").textContent;
+
+  if (selectedAnswer === currentQuiz.choices[currentQuiz.answer]) {
+    score++;
+    console.log(score);
+  }
+
+  // Add a class of "selected" to the chosen choice
+  selectedChoice?.classList.add("selected");
+
+  // Disable further selections while transitioning to the next question
+  choices.forEach((choice) => {
+    choice.removeEventListener("click", handleChoiceSelection);
+  });
+
+  // Delay before loading the next question
+  setTimeout(() => {
+    // console.log(currentQuestion);
+    currentQuestion++;
+
+    if (currentQuestion === quizData.length) {
+      showSummaryPage();
+    } else if (currentQuestion % 4 === 0 && currentQuestion >= 4) {
+      selectedChoice.classList.remove("selected");
+      showSummaryPage();
+    } else {
+      selectedChoice.classList.remove("selected");
+      loadQuestion();
+    }
+
+    // Enable selections for the next question
+    choices.forEach((choice) => {
+      choice.addEventListener("click", handleChoiceSelection);
+    });
+  }, 500); // 0.5 second delay
+}
+
+// Function to display the summary page
+function showSummaryPage() {
+  questionText.textContent = "";
+  choicesContainer.style.display = "none";
+  summaryContainer.style.display = "block";
+
+  // Update the summary page with the details
+  summaryContainer.innerHTML = `You have answered ${score} out of ${currentQuestion} correctly
+  `;
+  buttonContainer.onclick = startNewQuiz;
+}
+
+// Function to handle starting a new quiz
+function startNewQuiz() {
+  choicesContainer.style.display = "block";
+  summaryContainer.style.display = "none";
+  //   currentQuestion = 0;
+  loadQuestion();
+}
+
+// Add event listeners to choices
+choicesContainer.addEventListener("click", handleChoiceSelection);
+
+// Initial question load
+loadQuestion();
